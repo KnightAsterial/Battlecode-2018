a23e6be299e052a7c0dddb299b1c265e186c4f04
19bmiles <19bmiles@gmail.com>
2018-01-15 17:38:12 -0500

    Random Movement

--- bryceTestBot/Player.java
+++ bryceTestBot/Player.java
@@ -1,14 +1,12 @@
 // import the API.
 // See xxx for the javadocs.
+import java.util.Random;
+
 import bc.*;
 
 public class Player {
 
     public static void main(String[] args) {
-        // You can use other files in this directory, and in subdirectories.
-        Extra extra = new Extra(27);
-        System.out.println(extra.toString());
-
 
         // MapLocation is a data structure you'll use a lot.
         MapLocation loc = new MapLocation(Planet.Earth, 10, 20);
@@ -24,17 +22,33 @@ public class Player {
 
         // Direction is a normal java enum.
         Direction[] directions = Direction.values();
-
+        
+        Random generator = new Random();
+        int n = generator.nextInt(8);
+        Direction dirs[] = new Direction[8];
+        dirs[0] = Direction.East;
+        dirs[1] = Direction.North;
+        dirs[2] = Direction.Northeast;
+        dirs[3] = Direction.Northwest;
+        dirs[4] = Direction.South;
+        dirs[5] = Direction.Southeast;
+        dirs[6] = Direction.Southwest;
+        dirs[7] = Direction.West;
+        
         while (true) {
             System.out.println("Current round: "+gc.round());
             // VecUnit is a class that you can think of as similar to ArrayList<Unit>, but immutable.
             VecUnit units = gc.myUnits();
             for (int i = 0; i < units.size(); i++) {
                 Unit unit = units.get(i);
-
-                // Most methods on gc take unit IDs, instead of the unit objects themselves.
-                if (gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), Direction.Southeast)) {
-                    gc.moveRobot(unit.id(), Direction.Southeast);
+                
+                while (gc.isMoveReady(unit.id()))
+                {
+	                // Most methods on gc take unit IDs, instead of the unit objects themselves.
+	                if (gc.canMove(unit.id(), dirs[n])) {
+	                    gc.moveRobot(unit.id(), dirs[n]);
+	                }
+	                n = generator.nextInt(8);
                 }
             }
             // Submit the actions we've done, and wait for our next turn.
